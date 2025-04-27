c16c88b8fa1e445771f87476fb2f3b8f2ea307be
Omar Saad <201911101@ua.edu.lb>
2025-04-28 01:46:50 +0300

    asdzx

--- index.js
+++ index.js
@@ -171,9 +171,8 @@ async function fetchElo(playerId) {
         }
 
         // Launch Puppeteer with Chrome path
-        browser = await puppeteer.launch({
+        const browser = await puppeteer.launch({
             headless: true,
-            executablePath: '/usr/bin/google-chrome',  // Path to your chrome.exe
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
 
